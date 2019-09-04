7ff8ce4dd4a55f047969a1c76b337d11d7ccbbef
chenbangyuan <chengbangyuan@sczdyk.com>
2019-09-04 14:06:19 +0800

    添加

--- spring-config/src/main/java/Application.java
+++ spring-config/src/main/java/Application.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication(scanBasePackages  = {"com.spring.cloud"})
 @EnableEurekaClient
 @EnableConfigServer
-
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

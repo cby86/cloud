2c3dae79af50ec3c09ff597d960d329afeafba0b
chenbangyuan <chengbangyuan@sczdyk.com>
2019-09-05 16:00:44 +0800

    参数自动刷新

--- spring-order/src/main/java/Application.java
+++ spring-order/src/main/java/Application.java
@@ -2,6 +2,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients(basePackages = {"com.spring.cloud.feign"})
 @EnableHystrix
 @EnableCircuitBreaker
+@RefreshScope
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

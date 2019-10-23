23b4634c331a89758095f539c3ed084fcd920a6b
chenbangyuan <chengbangyuan@sczdyk.com>
2019-10-23 13:11:03 +0800

    网关授权

--- spring-turbine/src/main/java/Application.java
+++ spring-turbine/src/main/java/Application.java
@@ -1,7 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 

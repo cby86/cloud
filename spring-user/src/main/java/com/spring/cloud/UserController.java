a363f1b19328852ae6749d72364d47fb26e59e36
chenbangyuan <chengbangyuan@sczdyk.com>
2019-10-23 13:11:44 +0800

    网关授权

--- spring-user/src/main/java/com/spring/cloud/UserController.java
+++ spring-user/src/main/java/com/spring/cloud/UserController.java
@@ -1,16 +1,8 @@
 package com.spring.cloud;
 import com.spring.cloud.utils.RequestUserUtils;
 import org.springframework.security.User;
-import com.spring.cloud.utils.SecurityUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.support.RequestContext;
-import org.springframework.web.servlet.support.RequestContextUtils;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/")

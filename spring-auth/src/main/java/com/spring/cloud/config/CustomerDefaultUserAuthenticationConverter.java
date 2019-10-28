e2ceb1b2e4ae6c6d3fc9516f17a9368f2ef799fd
chenbangyuan <chengbangyuan@sczdyk.com>
2019-10-28 16:23:48 +0800

    线程队列

---
+++ spring-auth/src/main/java/com/spring/cloud/config/CustomerDefaultUserAuthenticationConverter.java
@@ -0,0 +1,25 @@
+package com.spring.cloud.config;
+import org.springframework.security.SecurityUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CustomerDefaultUserAuthenticationConverter extends DefaultUserAuthenticationConverter {
+    private static final String IDENTIFY = "identify";
+    public Map<String, ?> convertUserAuthentication(Authentication authentication) {
+        Map<String, Object> response = new LinkedHashMap<String, Object>();
+        response.put(IDENTIFY, ((SecurityUser) authentication.getPrincipal()).getId());
+        response.put(USERNAME, authentication.getName());
+        if (authentication.getAuthorities() != null && !authentication.getAuthorities().isEmpty()) {
+            response.put(AUTHORITIES, AuthorityUtils.authorityListToSet(authentication.getAuthorities()));
+        }
+        return response;
+    }
+}
---
+++ spring-zuul/src/main/java/com/spring/cloud/AuthPreRequestFilter.java
@@ -0,0 +1,45 @@
+package com.spring.cloud;
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import com.spring.cloud.utils.SecurityUtils;
+import com.spring.cloud.utils.RequestUserUtils;
+import org.springframework.security.SecurityUser;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 统一权限认证
+ */
+public class AuthPreRequestFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        System.err.println(request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        SecurityUser user = SecurityUtils.currentUser();
+        if (user != null) {
+            currentContext.addZuulRequestHeader(RequestUserUtils.USERHEADERKEY, JSONObject.toJSON(user).toString());
+        }
+        return null;
+    }
+}

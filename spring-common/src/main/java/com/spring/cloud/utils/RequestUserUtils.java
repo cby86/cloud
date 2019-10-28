afaa3b418e233a29002eefc25929f7ff075a627b
chenbangyuan <chengbangyuan@sczdyk.com>
2019-10-28 14:40:57 +0800

    获取用户

--- spring-common/src/main/java/com/spring/cloud/utils/RequestUserUtils.java
+++ spring-common/src/main/java/com/spring/cloud/utils/RequestUserUtils.java
@@ -21,6 +21,7 @@ public class RequestUserUtils {
             }
             SecurityUser user = new SecurityUser();
             user.setUsername(username);
+            user.setId(jsonObject.getString("identify"));
             JSONArray authorities = jsonObject.getJSONArray("authorities");
             for (Object obj : authorities) {
                 user.addAuthority(obj.toString());

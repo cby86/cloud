accae2f8a857fe9daf420d3859af00d436892ae2
panyuanjun <1172190237@qq.com>
2019-11-06 16:37:34 +0800

    111

--- spring-eureka-server/src/main/java/Application.java
+++ spring-eureka-server/src/main/java/Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 }

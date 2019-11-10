d2050e2e9d34639c783c3e2ab9c29b4845388a61
cby86 <cby_wxy@126.com>
2019-11-10 19:49:56 +0800

    route

--- spring-gateway/src/main/java/com/spring/cloud/controller/FallbackController.java
+++ spring-gateway/src/main/java/com/spring/cloud/controller/FallbackController.java
@@ -1,4 +1,8 @@
 package com.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
@@ -16,13 +20,21 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/fallback")
+@RequestMapping("")
+@RefreshScope
 public class FallbackController {
-    @RequestMapping("")
-    public Map<String, Object> fallback(ServerWebExchange exchange,BindingContext bindingContext){
+    @Value("${returnFailRoutePath:false}")
+    private boolean returnFailRoutePath = false;
+
+    @RequestMapping("/fallback")
+    public Map<String, Object> fallback(ServerWebExchange exchange, BindingContext bindingContext) {
+        Object attribute = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ORIGINAL_REQUEST_URL_ATTR);
         Map<String, Object> data = new HashMap<>();
         data.put("status", 0);
         data.put("message", "系统繁忙，请稍后再试");
+        if (returnFailRoutePath) {
+            data.put("data", attribute.toString());
+        }
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(HttpStatus.SERVICE_UNAVAILABLE);
         return data;

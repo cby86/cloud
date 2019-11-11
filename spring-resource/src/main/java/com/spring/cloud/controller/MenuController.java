afa7582bfcd63a304761ab3b9d3bb6aac32f6e7f
panyuanjun <1172190237@qq.com>
2019-11-11 13:37:22 +0800

    refactor

---
+++ spring-resource/src/main/java/com/spring/cloud/controller/MenuController.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.controller;
+
+import com.spring.cloud.base.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author panyuanjun
+ * @Date 2019/11/11/011 11:24
+ **/
+@RestController
+@RequestMapping("/menu")
+public class MenuController extends BaseController {
+
+
+}
--- spring-resource/src/main/java/com/spring/cloud/controller/RoleController.java
+++ spring-resource/src/main/java/com/spring/cloud/controller/RoleController.java
@@ -1,26 +1,46 @@
 package com.spring.cloud.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.spring.cloud.base.BaseController;
 import com.spring.cloud.entity.Role;
-import com.spring.cloud.repository.RoleRepository;
+import com.spring.cloud.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 角色接口
+ *
+ * @author
+ */
 @RestController
-@RequestMapping("/")
-public class RoleController {
+@RequestMapping("/role")
+public class RoleController extends BaseController {
+
     @Autowired
-    RoleRepository roleRepository;
+    RoleService roleService;
+
     @RequestMapping("/findRoles")
-    public String findRoles() {
-        return "";
+    public Map<String, Object> findRoles() {
+        List<Role> roleList = roleService.findRoleList();
+        return this.resultMap("0", "success", roleList);
     }
 
     @RequestMapping("/findRoleById")
     public Role findRoleById(String roleId) {
-        Role role = roleRepository.findRoleById(roleId);
-        return role;
+        return roleService.findRoleById(roleId);
     }
 
+
+    @RequestMapping("/findRoles")
+    public Map<String, Object> save(String id, String code, String menuIds) {
+        roleService.saveOrUpdate(id, code, menuIds);
+        return this.resultMap("0", "success", null);
+    }
 }
 

165f7ccf296adbc978bbaba3a7ae56e4b7c8ad68
chenbangyuan <chengbangyuan@sczdyk.com>
2019-11-15 10:16:20 +0800

    clear

--- spring-backend-center/src/main/java/com/spring/cloud/controller/OrderController.java
+++ spring-backend-center/src/main/java/com/spring/cloud/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.SecurityUser;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ public class OrderController {
     private DiscoveryClient discoveryClient;
 
     @Autowired
+    @LoadBalanced
     private RestTemplate restTemplate;
 
 
--- spring-resource/src/main/java/com/spring/cloud/controller/MenuController.java
+++ spring-resource/src/main/java/com/spring/cloud/controller/MenuController.java
@@ -4,10 +4,12 @@ import com.spring.cloud.base.BaseController;
 import com.spring.cloud.entity.Menu;
 import com.spring.cloud.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -37,7 +39,24 @@ public class MenuController extends BaseController {
     }
 
     @RequestMapping("/register")
-    public Map<String, Object> register(@RequestBody  Map<String,Object> resource) {
+    public Map<String, Object> register(@RequestBody  Map<String,List<Map>> resource) {
+        List<Menu> menus = new ArrayList<>();
+        for (Map.Entry<String, List<Map>> entry : resource.entrySet()) {
+            String url = entry.getKey();
+            List<Map> desc = entry.getValue();
+            Menu menu = new Menu();
+            for (Map<String, String> data : desc) {
+                String model = data.get("model");
+                if (StringUtils.isEmpty(model)) {
+                    menu.setName(model);
+                    menu.setMenuType(0);
+                }
+                String name = data.get("name");
+                String desc1 = data.get("desc");
+                String version = data.get("version");
+            }
+
+        }
         return this.resultMap("0", "success", null);
     }
 

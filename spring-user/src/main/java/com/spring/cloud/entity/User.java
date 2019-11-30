7f6f1aa69e9605792923c7aa18cf3accf85971b5
chenbangyuan <chengbangyuan@sczdyk.com>
2019-11-30 17:47:30 +0800

    config作为配置更新分发服务器

--- spring-user/src/main/java/com/spring/cloud/entity/User.java
+++ spring-user/src/main/java/com/spring/cloud/entity/User.java
@@ -14,7 +14,7 @@ public class User extends BaseEntity{
     private String password;
 
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "mb_hunter_user_role",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})

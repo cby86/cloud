c2e9414ced3dc71a1453808b2d05aad447c5ac91
chenbangyuan <chengbangyuan@sczdyk.com>
2019-11-30 17:56:58 +0800

    config作为配置更新分发服务器

--- spring-common/src/main/java/com/spring/cloud/support/mvc/ApiVersionCondition.java
+++ spring-common/src/main/java/com/spring/cloud/support/mvc/ApiVersionCondition.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.mvc.condition.RequestCondition;
 
+import java.math.BigDecimal;
+
 /**
  * 版本号规则配置类
  */
 public class ApiVersionCondition implements RequestCondition<ApiVersionCondition> {
-
+    //如果不传版本号，默认为0
+    private static final int DEFAULTVERSION = 0;
     private int apiVersion;
 
     public ApiVersionCondition(int apiVersion) {
@@ -24,7 +27,7 @@ public class ApiVersionCondition implements RequestCondition<ApiVersionCondition
     public ApiVersionCondition getMatchingCondition(HttpServletRequest request) {
         String ver = request.getHeader("Api-Version");
         if (StringUtils.isEmpty(ver)) {
-            return null;
+            ver = Integer.toString(DEFAULTVERSION);
         }
         //因为请求头里面传来的是小数，所以需要乘以10
         int version = (int) (Double.valueOf(ver) * 10);

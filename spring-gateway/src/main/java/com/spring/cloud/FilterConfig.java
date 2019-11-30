ba3c962651a3ee4342ab0ccc6621154ef64d0ad6
chenbangyuan <chengbangyuan@sczdyk.com>
2019-11-30 17:48:59 +0800

    config作为配置更新分发服务器

--- spring-gateway/src/main/java/com/spring/cloud/FilterConfig.java
+++ spring-gateway/src/main/java/com/spring/cloud/FilterConfig.java
@@ -33,26 +33,4 @@ public class FilterConfig {
             }));
         };
     }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-//        Flux.merge(Flux.range(0,5),Flux.range(2,5)).subscribe(System.out::println);
-//        Flux.range(1, 10).reduceWith(() -> 10, (x, y) -> x + y).subscribe(System.out::println);
-//        List<String> df = Flux.just("df").toStream().collect(Collectors.toList());
-//        List<Object> t = Flux.empty().concatWith(Mono.just("t")).toStream().collect(Collectors.toList());
-//        t.stream().forEach(System.out::println);
-//        Mono.empty().concatWith(Mono.just("t")).then();
-//        Mono.just("t").map(t -> t + "tt").flatMap(t -> {
-//            System.out.println(t);
-//            return Mono.empty().then(Mono.fromRunnable(() -> {
-//                System.err.println("first post filter");
-//            }));
-//        }).subscribe();
-//        Mono.just("dd").map(d->{
-//            return d + "dfas";
-//        }).switchIfEmpty(Mono.just("d")).subscribe(System.out::println);
-        Mono<String> stringMono = Mono.just("t").flatMap(s -> Mono.subscriberContext().map(c -> s + c.get("test"))).subscriberContext(ctx -> ctx.put("test", "World"));
-        stringMono.subscribe(System.out::println);
-    }
-
-
 }

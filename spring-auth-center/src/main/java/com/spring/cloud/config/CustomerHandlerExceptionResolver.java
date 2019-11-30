4c0232ba0dd59b93fe348a27ce06944ba2e19693
chenbangyuan <chengbangyuan@sczdyk.com>
2019-11-30 17:35:23 +0800

    config作为配置更新分发服务器

--- spring-auth-center/src/main/java/com/spring/cloud/config/CustomerHandlerExceptionResolver.java
+++ spring-auth-center/src/main/java/com/spring/cloud/config/CustomerHandlerExceptionResolver.java
@@ -1,27 +1,30 @@
 package com.spring.cloud.config;
-import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.annotation.Annotation;
 
 @Component
-public class CustomerHandlerExceptionResolver implements HandlerExceptionResolver ,Order{
+public class CustomerHandlerExceptionResolver implements HandlerExceptionResolver, Ordered {
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        if (ex instanceof InternalAuthenticationServiceException) {
+            ModelAndView modelAndView = new ModelAndView(new MappingJackson2JsonView());
+            modelAndView.addObject("status", 0);
+            modelAndView.addObject("message", "用户名或密码错误");
+            return modelAndView;
+        }
         return null;
     }
 
-    @Override
-    public int value() {
-        return -1;
-    }
 
     @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
+    public int getOrder() {
+        return 0;
     }
 }

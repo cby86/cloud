cee7c94b544b4bcea366c07eeca77dd4bd49181e
chenbangyuan <chengbangyuan@sczdyk.com>
2019-12-06 16:36:04 +0800

    网关动态权限加载和拦截

--- spring-gateway/src/main/java/com/spring/cloud/config/ResourceEndpointConfig.java
+++ spring-gateway/src/main/java/com/spring/cloud/config/ResourceEndpointConfig.java
@@ -1,11 +1,13 @@
 package com.spring.cloud.config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(CustomerReactiveAuthorizationManager.class)
 public class ResourceEndpointConfig{
     @Bean
-    public ResourcePermitRefreshMvcEndpoint getResourcePermitManager() {
-        return new ResourcePermitRefreshMvcEndpoint();
+    public ResourcePermitRefreshMvcEndpoint getResourcePermitManager(CustomerReactiveAuthorizationManager customerReactiveAuthorizationManager) {
+        return new ResourcePermitRefreshMvcEndpoint(customerReactiveAuthorizationManager);
     }
 }
--- spring-gateway/src/main/java/com/spring/cloud/config/ResourcePermitRefreshMvcEndpoint.java
+++ spring-gateway/src/main/java/com/spring/cloud/config/ResourcePermitRefreshMvcEndpoint.java
@@ -1,12 +1,15 @@
 package com.spring.cloud.config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 
 @Endpoint(id="resourcePermitInfo")
 public class ResourcePermitRefreshMvcEndpoint  {
-    @Autowired
     CustomerReactiveAuthorizationManager customerReactiveAuthorizationManager;
+
+    public ResourcePermitRefreshMvcEndpoint(CustomerReactiveAuthorizationManager customerReactiveAuthorizationManager) {
+        this.customerReactiveAuthorizationManager = customerReactiveAuthorizationManager;
+    }
+
     @ReadOperation
     public String resourcePermitInfo() {
         return customerReactiveAuthorizationManager.toString();

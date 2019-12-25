06b67a0761730bb778261876c9f40f7e55d48d56
chenbangyuan <chengbangyuan@sczdyk.com>
2019-12-25 16:35:55 +0800

    add docker

--- spring-eureka-server/src/main/docker/runshell.sh
+++ spring-eureka-server/src/main/docker/runshell.sh
@@ -1,15 +1,14 @@
-#docker 镜像/容器名字或者jar名字 这里都命名为这个
 SERVER_NAME=spring-eureka-server
 #删除正在运行容器
-for line in `docker ps | grep "$SERVER_NAME" | awk '{print $1}'`
+for line in `docker ps| grep "$SERVER_NAME" | awk '{print $1}'`
 do
-  docker stop $line
-  echo "停止容器"+$line
-  docker rm $line
-  echo "删除容器"+$line
+ docker stop $line
+ echo "停止容器"+$line
+ docker rm $line
+ echo "删除容器"+$line
 done
 
-#如果同一版本多次发布,需要把之前的版本清楚
+
 for line in `docker images | grep "<none>" | awk '{print $3}'`
 do
   docker rmi $line

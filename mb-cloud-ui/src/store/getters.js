dd8d1b902a773c52f94f9067c90be0be68ec68c7
chenbangyuan <chengbangyuan@sczdyk.com>
2020-02-17 16:50:45 +0800

    401 处理

--- mb-cloud-ui/src/store/getters.js
+++ mb-cloud-ui/src/store/getters.js
@@ -1,4 +1,7 @@
 export default {
   user: state => state.user,
   authentications: state => state.authentications,
+  hasAuth:(state) => (name) => {
+    return state.authentications.filter(item => item.url === name).length>0
+  }
 }

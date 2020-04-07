a864e631c993674aa37efba9b6cce3bca7f210fa
cby86 <cby_wxy@126.com>
2020-04-07 15:49:29 +0800

    日志全局模块定义

--- spring-eureka-server/src/main/java/Application.java
+++ spring-eureka-server/src/main/java/Application.java
@@ -1,12 +1,15 @@
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(scanBasePackages  = {"com.spring.cloud"})
 @EnableEurekaServer
+@Slf4j
 public class Application {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.debug("dfasdf");
     }
 }

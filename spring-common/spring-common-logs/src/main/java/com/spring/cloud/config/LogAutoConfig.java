ba4d7393935aa0acd4d6e6135870fbf65a586acd
cby86 <cby_wxy@126.com>
2020-04-09 13:18:55 +0800

    logstash log support

---
+++ spring-common/spring-common-logs/src/main/java/com/spring/cloud/config/LogAutoConfig.java
@@ -0,0 +1,10 @@
+package com.spring.cloud.config;
+
+import ch.qos.logback.core.spi.AppenderAttachable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogAutoConfig {
+
+}
--- spring-common/spring-common-redis/src/main/java/com/spring/cloud/utils/RedisSubscribeCallback.java
+++
@@ -1,6 +0,0 @@
-package com.spring.cloud.utils;
-
- 
-public interface RedisSubscribeCallback {
-    void callback(String msg);
-}
